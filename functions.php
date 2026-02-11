cbf721325ad64151680e258f2f861f670ff6d4bf
Ashley Bill <abill@ritchiebros.com>
2026-02-11 00:45:15 -0700

    clean up old files

--- functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Enqueue parent theme styles
-add_action('wp_enqueue_scripts', 'ember_dawn_child_enqueue_styles');
-function ember_dawn_child_enqueue_styles() {
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-}
